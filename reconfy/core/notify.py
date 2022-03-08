dee30171ea84ed441efb94c7e942378a11503c35
Américo Júnior <eu@americojunior.com>
2022-03-08 20:30:41 +0200

    Fix errors

--- reconfy/core/notify.py
+++
@@ -1,7 +0,0 @@
-from discord_webhook import DiscordWebhook
-
-
-def discord(webhook_url, content):
-    webhook = DiscordWebhook(url=webhook_url, content=content)
-    response = webhook.execute()
-    return response
